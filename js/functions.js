4e2c29a3d0a351d9ee23ea240d4c662b6791a5ed
Taboada Lola <produlola@gmail.com>
2023-01-30 17:22:16 -0300

    final

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById('boton').onclick = function name() {
+    console.log ("Capturamos el evento clcik");
+    document.getElementById("demo").innerHTML = "GRACIAS POR VER COMO FUNCIONA MI PRIMER BOTON"
+    
+}
+
+document.getElementById('boton').onclick = function name() {
+    console.log ("Capturamos el evento clcik");
+    document.getElementById("demo").innerHTML = "GRACIAS POR VER COMO FUNCIONA MI PRIMER BOTON"
+    
+}
+
+
